3278f78ee083e2a9ab7048ae6b899142d7d84509
ashwin153 <ashwin.madavan@gmail.com>
2015-07-22 14:25:38 -0700

    Fixed genetic erros

--- src/main/java/com/ashwin/evolve/genetic/chromosomes/SortingChromosome.java
+++ src/main/java/com/ashwin/evolve/genetic/chromosomes/SortingChromosome.java
@@ -2,7 +2,6 @@ package com.ashwin.evolve.genetic.chromosomes;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 import com.ashwin.evolve.genetic.GeneticChromosome;
 import com.ashwin.evolve.programs.Instruction;
@@ -25,56 +24,42 @@ import com.ashwin.evolve.programs.operands.IndirectOperand;
 public class SortingChromosome extends Program implements GeneticChromosome<SortingChromosome> {
 
 	private Memory _in, _wk;
-	private int[][] _tests;
+	private double _fitness;
 	
-	/**
-	 * Constructs a program chromosome with the specified number of instructions.
-	 * 
-	 * @param length
-	 */
-	public SortingChromosome(int length, Properties props) {
-		_numInstrs = Integer.valueOf(props.getProperty("ga.sorting.instrs"));
-		_numTests  = Integer.valueOf(props.getProperty("ga.sorting.tests"));
-		_numInputs = Integer.valueOf(props.getProperty("ga.sorting.inputs"));
+	public SortingChromosome(int length, Memory in, Memory wk) throws ExecutionException {
+		super(new ArrayList<Instruction>());
 		
-		_in = new Memory("in", _numInputs, 0);
-		_wk = new Memory("wk", 5, 0);
-		_wk.write(0, _numInputs);
+		_in = in;
+		_wk = wk;
 		
-		// Regenerate the test cases for each new chromosome. This
-		// ensures that the optimal chromosome can handle arbitrary inputs.
-		_tests = new int[_numTests][_numInputs];
-		for (int i = 0; i < _tests.length; i++)
-			for (int j = 0; j < _tests[i].length; j++)
-				_tests[i][j] = (int) (Math.random() * Integer.MAX_VALUE);
-	}
-	
-	public SortingChromosome(List<Instruction> instructions) {
-		super(instructions);
-	}
-
-	@Override
-	public double fitness() {
-		double fitness = 0;
-		for (int[] test : _tests) {
+		// Generate a new set of random instructions
+		for(int i = 0; i < length; i++)
+			getInstructions().add(random(i, length));
+		
+		// Calculate the fitness of this chromosome using the number of inversions it is 
+		// able to "fix" in a set of randomly generated numbers
+		for (int i = 0; i < 50; i++) {
+			for (int j = 0; j < in.size(); j++)
+				_in.write(j, (int) (Math.random() * Integer.MAX_VALUE));
+			
 			try {
-				_in.write(0, test);
+				// Calculate the total number of inversions before and after execution
 				int before = inversions(_in);
 				execute();
-				int after = inversions(_in);
-				
-				// Reward sorting programs that successfully sort inputs
-				fitness += before - after;
-				if(after == 0)
-					fitness += _tests.length * 20;
-				
+				int after  = inversions(_in);
+				_fitness  += (before - after) / before;
 			} catch (ExecutionException e) {
 				// Penalize sorting programs that do not terminate
-				fitness -= _tests.length * 20;
+				_fitness -= _in.size() * 20;
 			}
 		}
+	}
+	
+	public SortingChromosome(List<Instruction> instructions, Memory in, Memory wk) {
+		super(instructions);
 		
-		return fitness;
+		_in = in;
+		_wk = wk;
 	}
 	
 	/**
@@ -100,6 +85,34 @@ public class SortingChromosome extends Program implements GeneticChromosome<Sort
 		return inversions;
 	}
 	
+	public Instruction random(int index, int length) {
+		switch((int) (Math.random() * 7)) {
+			case  0: return new Dec  (new DirectOperand(_wk));
+			case  1: return new Inc  (new DirectOperand(_wk));
+			case  2: return new Mov  (new DirectOperand(_in), new DirectOperand(_wk));
+			case  3: return new Mov  (new DirectOperand(_wk), new DirectOperand(_wk));
+			case  4: return new Mov  (new ImmediateOperand(-5, 5), new DirectOperand(_wk));
+			case  5: return new Xchg (new IndirectOperand(_wk, _in), new IndirectOperand(_wk, _in));
+			default:
+				switch((int) (Math.random() * 9)) {
+					case  0: return new Jeq  (new ImmediateOperand(-index, length - index), new DirectOperand(_wk), new DirectOperand(_wk));
+					case  1: return new Jg   (new ImmediateOperand(-index, length - index), new DirectOperand(_wk), new DirectOperand(_wk));
+					case  2: return new Jl   (new ImmediateOperand(-index, length - index), new DirectOperand(_wk), new DirectOperand(_wk));
+					case  3: return new Jne  (new ImmediateOperand(-index, length - index), new DirectOperand(_wk), new DirectOperand(_wk));
+					case  4: return new Jeq  (new ImmediateOperand(-index, length - index), new IndirectOperand(_wk, _in), new IndirectOperand(_wk, _in));
+					case  5: return new Jg   (new ImmediateOperand(-index, length - index), new IndirectOperand(_wk, _in), new IndirectOperand(_wk, _in));
+					case  6: return new Jl   (new ImmediateOperand(-index, length - index), new IndirectOperand(_wk, _in), new IndirectOperand(_wk, _in));
+					case  7: return new Jne  (new ImmediateOperand(-index, length - index), new IndirectOperand(_wk, _in), new IndirectOperand(_wk, _in));
+					default: return new Jmp  (new ImmediateOperand(-index, length - index));
+				}
+		}
+	}
+	
+	@Override
+	public double fitness() {
+		return _fitness;
+	}
+	
 	@Override
 	public SortingChromosome crossover(SortingChromosome mate, double rate) {
 		List<Instruction> i1 = new ArrayList<Instruction>(this.getInstructions());
@@ -116,7 +129,7 @@ public class SortingChromosome extends Program implements GeneticChromosome<Sort
 			i1.addAll(l, i2.subList(l, l + h));
 		}
 		
-		return new SortingChromosome(i1);
+		return new SortingChromosome(i1, _in, _wk);
 	}
 
 	@Override
@@ -126,30 +139,6 @@ public class SortingChromosome extends Program implements GeneticChromosome<Sort
 //			if(Math.random() < rate)
 //				instr.set(i, _config.getRandomInstruction(i, instr.size()));
 		return null;
-	}
-	
-//	public SortingChromosome random() {
-//		switch((int) (Math.random() * 7)) {
-//				case  0: instrs.add(new Dec  (new DirectOperand(_wk))); break;
-//				case  1: instrs.add(new Inc  (new DirectOperand(_wk))); break;
-//				case  2: instrs.add(new Mov  (new DirectOperand(_in), new DirectOperand(_wk))); break;
-//				case  3: instrs.add(new Mov  (new DirectOperand(_wk), new DirectOperand(_wk))); break;
-//				case  4: instrs.add(new Mov  (new ImmediateOperand(-5, 5), new DirectOperand(_wk))); break;
-//				case  5: instrs.add(new Xchg (new IndirectOperand(_wk, _in), new IndirectOperand(_wk, _in))); break;
-//				default:
-//					switch((int) (Math.random() * 9)) {
-//						case  0: instrs.add(new Jeq  (new ImmediateOperand(-i, _numInstrs - i), new DirectOperand(_wk), new DirectOperand(_wk)));
-//						case  1: instrs.add(new Jg   (new ImmediateOperand(-i, _numInstrs - i), new DirectOperand(_wk), new DirectOperand(_wk))); break;
-//						case  2: instrs.add(new Jl   (new ImmediateOperand(-i, _numInstrs - i), new DirectOperand(_wk), new DirectOperand(_wk))); break;
-//						case  3: instrs.add(new Jne  (new ImmediateOperand(-i, _numInstrs - i), new DirectOperand(_wk), new DirectOperand(_wk))); break;
-//						case  4: instrs.add(new Jeq  (new ImmediateOperand(-i, _numInstrs - i), new IndirectOperand(_wk, _in), new IndirectOperand(_wk, _in))); break;
-//						case  5: instrs.add(new Jg   (new ImmediateOperand(-i, _numInstrs - i), new IndirectOperand(_wk, _in), new IndirectOperand(_wk, _in))); break;
-//						case  6: instrs.add(new Jl   (new ImmediateOperand(-i, _numInstrs - i), new IndirectOperand(_wk, _in), new IndirectOperand(_wk, _in))); break;
-//						case  7: instrs.add(new Jne  (new ImmediateOperand(-i, _numInstrs - i), new IndirectOperand(_wk, _in), new IndirectOperand(_wk, _in))); break;
-//						default: instrs.add(new Jmp  (new ImmediateOperand(-i, _numInstrs - i)));
-//					}
-//			}
-//		}
 	}
 
 }
